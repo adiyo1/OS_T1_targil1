cb540e62de37d44e0022f8f752b3ec33dc2e459f
Yohanan Adi <155353862+adiyo1@users.noreply.github.com>
2024-05-16 22:56:01 +0300

    Add files via upload

--- targil6/findPhone.c
+++ targil6/findPhone.c
@@ -1,92 +1,212 @@
 
+// #include <unistd.h>
+// #include <stdlib.h>
+// #include <stdio.h>
+// #include <sys/wait.h>
+// #include <string.h>
+
+// #define DEFAULT_PHONEBOOK_FILE "phonebook.txt" // Assuming phonebook.txt is in the same directory
+
+// int main(int argc, char *argv[])
+// {
+//   if (argc < 2 || argc > 3)
+//   {
+//     fprintf(stderr, "Usage: %s <name> [<phonebook_file>]\n", argv[0]);
+//     exit(1);
+//   }
+
+//   char *name = argv[1];
+//   char *phonebook_file = (argc == 3) ? argv[2] : DEFAULT_PHONEBOOK_FILE;
+
+//   int pipe1[2], pipe2[2]; // Two pipes for chaining commands
+//   if (pipe(pipe1) == -1 || pipe(pipe2) == -1)
+//   {
+//     perror("pipe");
+//     exit(1);
+//   }
+
+//   int pid1 = fork(); // First child process (grep)
+//   if (pid1 == -1)
+//   {
+//     perror("fork");
+//     exit(1);
+//   }
+
+//   if (pid1 == 0)
+//   {
+//     dup2(pipe1[1], STDOUT_FILENO); // Redirect grep output to pipe1
+//     close(pipe1[0]);               // Close unused read end of pipe1
+//     close(pipe2[0]);               // Close unused read and write ends of pipe2
+//     close(pipe2[1]);
+
+//     // Search for the entire line containing the name (case-insensitive)
+//     execlp("grep", "grep", "-i", name, phonebook_file, NULL);
+
+//     perror("execlp");
+//     exit(1);
+//   }
+//   else
+//   {
+//     int pid2 = fork(); // Second child process (awk)
+//     if (pid2 == -1)
+//     {
+//       perror("fork");
+//       exit(1);
+//     }
+
+//     if (pid2 == 0)
+//     {
+//       dup2(pipe1[0], STDIN_FILENO);  // Redirect awk input from pipe1
+//       dup2(pipe2[1], STDOUT_FILENO); // Redirect awk output to pipe2
+//       close(pipe1[1]);               // Close unused write end of pipe1
+//       close(pipe2[0]);               // Close unused read end of pipe2
+
+//       // Print only the second field (phone number) after splitting by comma
+//       execlp("awk", "awk", "-F,", "{print $2}", NULL);
+
+//       perror("execlp");
+//       exit(1);
+//     }
+//     else
+//     {                  // Parent process
+//       close(pipe1[0]); // Close unused read ends
+//       close(pipe1[1]);
+//       close(pipe2[1]); // Close unused write end
+
+//       int status;
+//       waitpid(pid1, &status, 0); // Wait for grep (pid1)
+//       waitpid(pid2, &status, 0); // Wait for awk (pid2)
+
+//       if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+//       {
+//         // Both grep and awk succeeded, read phone number
+//         printf("Phone number: ");
+//         char buffer[12]; // Assuming max phone number length is 9 digits + 1 separator
+//         ssize_t bytes_read = read(pipe2[0], buffer, sizeof(buffer) - 1);
+//         if (bytes_read == 0)
+//         {
+//           fprintf(stderr, "Name not found in phonebook\n");
+//         }
+//         else
+//         {
+//           buffer[strcspn(buffer, "\n")] = '\0'; // Remove trailing newline
+//           printf("%s\n", buffer);
+//         }
+//       }
+//       else
+//       {
+//         fprintf(stderr, "Name not found in phonebook\n");
+//       }
+//     }
+//   }
+
+//   return 0;
+// }
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/wait.h>
-#include <string.h>
-
-
 
 #define DEFAULT_PHONEBOOK_FILE "phonebook.txt" // Assuming phonebook.txt is in the same directory
 
+// Function to find the length of a string
+size_t my_strlen(const char *str) {
+    size_t len = 0;
+    while (str[len] != '\0') {
+        len++;
+    }
+    return len;
+}
 
+// Function to remove trailing newline character
+void remove_newline(char *str) {
+    size_t len = my_strlen(str);
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+    }
+}
 
 int main(int argc, char *argv[]) {
-  if (argc < 2 || argc > 3) {
-    fprintf(stderr, "Usage: %s <name> [<phonebook_file>]\n", argv[0]);
-    exit(1);
-  }
-
-  char *name = argv[1];
-  char *phonebook_file = (argc == 3) ? argv[2] : DEFAULT_PHONEBOOK_FILE;
-
-  int pipe1[2], pipe2[2]; // Two pipes for chaining commands
-  if (pipe(pipe1) == -1 || pipe(pipe2) == -1) {
-    perror("pipe");
-    exit(1);
-  }
-
-  int pid1 = fork(); // First child process (grep)
-  if (pid1 == -1) {
-    perror("fork");
-    exit(1);
-  }
-
-  if (pid1 == 0) {
-    dup2(pipe1[1], STDOUT_FILENO); // Redirect grep output to pipe1
-    close(pipe1[0]); // Close unused read end of pipe1
-    close(pipe2[0]); // Close unused read and write ends of pipe2
-    close(pipe2[1]);
-
-    // Search for the entire line containing the name (case-insensitive)
-    execlp("grep", "grep", "-i", name, phonebook_file, NULL);
-
-    perror("execlp");
-    exit(1);
-  } else {
-    int pid2 = fork(); // Second child process (awk)
-    if (pid2 == -1) {
-      perror("fork");
-      exit(1);
+    if (argc < 2 || argc > 3) {
+        write(STDERR_FILENO, "Usage: ", 7);
+        write(STDERR_FILENO, argv[0], my_strlen(argv[0]));
+        write(STDERR_FILENO, " <name> [<phonebook_file>]\n", 27);
+        exit(1);
     }
 
-    if (pid2 == 0) {
-      dup2(pipe1[0], STDIN_FILENO);  // Redirect awk input from pipe1
-      dup2(pipe2[1], STDOUT_FILENO); // Redirect awk output to pipe2
-      close(pipe1[1]); // Close unused write end of pipe1
-      close(pipe2[0]); // Close unused read end of pipe2
+    char *name = argv[1];
+    char *phonebook_file = (argc == 3) ? argv[2] : DEFAULT_PHONEBOOK_FILE;
 
-      // Print only the second field (phone number) after splitting by comma
-      execlp("awk", "awk", "-F,", "{print $2}", NULL);
+    int pipe1[2], pipe2[2]; // Two pipes for chaining commands
+    if (pipe(pipe1) == -1 || pipe(pipe2) == -1) {
+        perror("pipe");
+        exit(1);
+    }
 
-      perror("execlp");
-      exit(1);
-    } else { // Parent process
-      close(pipe1[0]); // Close unused read ends
-      close(pipe1[1]);
-      close(pipe2[1]); // Close unused write end
+    int pid1 = fork(); // First child process (grep)
+    if (pid1 == -1) {
+        perror("fork");
+        exit(1);
+    }
 
-      int status;
-      waitpid(pid1, &status, 0); // Wait for grep (pid1)
-      waitpid(pid2, &status, 0); // Wait for awk (pid2)
+    if (pid1 == 0) {
+        dup2(pipe1[1], STDOUT_FILENO); // Redirect grep output to pipe1
+        close(pipe1[0]);               // Close unused read end of pipe1
+        close(pipe2[0]);               // Close unused read and write ends of pipe2
+        close(pipe2[1]);
 
-      if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
-        // Both grep and awk succeeded, read phone number
-        printf("Phone number: ");
-        char buffer[12]; // Assuming max phone number length is 9 digits + 1 separator
-        ssize_t bytes_read = read(pipe2[0], buffer, sizeof(buffer) - 1);
-        if (bytes_read == 0) {
-          fprintf(stderr, "Name not found in phonebook\n");
-        } else {
-          buffer[strcspn(buffer, "\n")] = '\0'; // Remove trailing newline
-          printf("%s\n", buffer);
+        // Search for the entire line containing the name (case-insensitive)
+        execlp("grep", "grep", "-i", name, phonebook_file, NULL);
+
+        perror("execlp");
+        exit(1);
+    } else {
+        int pid2 = fork(); // Second child process (awk)
+        if (pid2 == -1) {
+            perror("fork");
+            exit(1);
         }
-      } else {
-        fprintf(stderr, "Name not found in phonebook\n");
-      }
-    }
-  }
 
-  return 0;
+        if (pid2 == 0) {
+            dup2(pipe1[0], STDIN_FILENO);  // Redirect awk input from pipe1
+            dup2(pipe2[1], STDOUT_FILENO); // Redirect awk output to pipe2
+            close(pipe1[1]);               // Close unused write end of pipe1
+            close(pipe2[0]);               // Close unused read end of pipe2
+
+            // Print only the second field (phone number) after splitting by comma
+            execlp("awk", "awk", "-F,", "{print $2}", NULL);
+
+            perror("execlp");
+            exit(1);
+        } else {                  // Parent process
+            close(pipe1[0]);      // Close unused read ends
+            close(pipe1[1]);
+            close(pipe2[1]);      // Close unused write end
+
+            int status;
+            waitpid(pid1, &status, 0); // Wait for grep (pid1)
+            waitpid(pid2, &status, 0); // Wait for awk (pid2)
+
+            if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+                // Both grep and awk succeeded, read phone number
+                write(STDOUT_FILENO, "Phone number: ", 14);
+                char buffer[12]; // Assuming max phone number length is 9 digits + 1 separator
+                ssize_t bytes_read = read(pipe2[0], buffer, sizeof(buffer) - 1);
+                if (bytes_read == 0) {
+                    write(STDERR_FILENO, "Name not found in phonebook\n", 28);
+                } else {
+                    buffer[bytes_read] = '\0'; // Null-terminate the string
+                    remove_newline(buffer);    // Remove trailing newline
+                    write(STDOUT_FILENO, buffer, my_strlen(buffer));
+                    write(STDOUT_FILENO, "\n", 1);
+                }
+            } else {
+                write(STDERR_FILENO, "Name not found in phonebook\n", 28);
+            }
+        }
+    }
+
+    return 0;
 }
 
